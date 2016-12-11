13868b82353d2f85642d5290cc922b14ab6763fc
Payton Quinn <paytonq@uw.edu>
2016-12-10 22:52:25 -0800

    first commit

---
+++ app/src/main/java/me/paytonq/app/stocktrack/MainAdapter.java
@@ -0,0 +1,88 @@
+package me.paytonq.app.stocktrack;
+
+
+import android.graphics.PorterDuffColorFilter;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+/**
+ * Created by paytonq on 11/5/2016.
+ */
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+    private List<String> mDataset;
+    private int tag;
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public CardView mTextView;
+        public ViewHolder(CardView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MainAdapter(List<String> myDataset) {
+        tag = 0;
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MainAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.text_view, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+
+        ViewHolder vh = new ViewHolder((CardView)v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        TextView t = (TextView) holder.mTextView.findViewById(R.id.card_title);
+        t.setText(mDataset.get(position));
+
+        /*final ViewHolder h = holder;
+        holder.mTextView.findViewById(R.id.image1).setOnClickListener(new View.OnClickListener() {
+            int innerTag = tag;
+            @Override
+            public void onClick(View v) {
+                mDataset.remove(h.getAdapterPosition());
+                notifyItemRemoved(h.getAdapterPosition());
+            }
+        });*/
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
---
+++ app/src/main/java/me/paytonq/app/stocktrack/PortfolioListView.java
@@ -0,0 +1,100 @@
+package me.paytonq.app.stocktrack;
+
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+
+/**
+ * Created by paytonq on 11/22/2016.
+ */
+
+public class PortfolioListView extends LinearLayout {
+    public enum ViewState {
+        LOADED, EMPTY, ERROR, LOADING
+    }
+
+
+    @BindView(R.id.my_recycler_view) RecyclerView rvItems;; // Your RecyclerView, which will display the items
+    TextView tvEmpty;     // Contains the message for empty
+    TextView tvError;     // Contains the message for an error
+    TextView tvLoading;   // Contains a message for loading
+
+    private RecyclerView.Adapter mAdapter;
+
+    private List<String> myDataset;
+    private RecyclerView.LayoutManager layoutManager;
+
+
+
+
+    public PortfolioListView(Context context) {
+        super(context);
+        init(context);
+    }
+
+
+    public PortfolioListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public PortfolioListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        // You'll need to define a view in layout/ that has the appropriate
+        // children views.
+
+        LayoutInflater.from(context).inflate(R.layout.activity_main, this, true);
+
+        // All the findViewById calls, setting up the items, adapter, etc
+
+        rvItems = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myDataset.add("Hello!");
+                mAdapter.notifyItemInserted(mAdapter.getItemCount());
+            }
+        });
+
+
+        myDataset = new ArrayList<String>();
+        // specify an adapter (see also next example)
+        mAdapter = new MainAdapter(myDataset);
+        layoutManager = new LinearLayoutManager(context);
+
+        rvItems.setLayoutManager(layoutManager);
+        rvItems.setAdapter(mAdapter);
+
+    }
+
+    public void updateViewState(ViewState state) {
+        switch (state) {
+            // Show and hide the views.
+
+        }
+    }
+
+    // More methods as you need them. Take a look in my project if you want to
+    // see how I did it, but try on your own first.
+
+}
